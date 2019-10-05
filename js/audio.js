646d142296538ebaf248b21a38290a32dbb60417
nicross <nicross@users.noreply.github.com>
2019-10-05 11:30:59 -0500

    Add noise buffer to audio module

--- js/audio.js
+++ js/audio.js
@@ -1,11 +1,20 @@
 const audio = (function IIFE() {
   const context = new AudioContext()
 
+  const noiseBufferSize = 4 * context.sampleRate
+  const noiseBuffer = context.createBuffer(1, noiseBufferSize, context.sampleRate)
+  const noiseBufferData = noiseBuffer.getChannelData(0)
+
+  for (let i = 0; i < noiseBufferSize; i += 1) {
+    noiseBufferData[i] = (2 * Math.random()) - 1
+  }
+
   return {
     activate: function () {
       context.resume()
       return this
     },
     context: () => context,
+    noiseBuffer: () => noiseBuffer,
   }
 })()
