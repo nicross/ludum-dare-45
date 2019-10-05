e18b45eefd6033e2d17f258587e01c4dacc4c3c1
nicross <nicross@users.noreply.github.com>
2019-10-04 22:42:16 -0500

    Expose audio context

--- js/audio.js
+++ js/audio.js
@@ -7,19 +7,6 @@ const audio = (function IIFE() {
       context.resume()
       return this
     },
-    connect: (node, channel = 0) => {
-      if (node instanceof AudioNode) {
-        node.connect(context, channel)
-      }
-
-      return this
-    },
-    disconnect: (node) => {
-      if (node instanceof AudioNode) {
-        node.disconnect(context)
-      }
-
-      return this
-    },
+    context: () => context,
   }
 })()
