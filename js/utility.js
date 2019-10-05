d100048a850ff7f5a9a28b7d6f0e6de2a37ca04e
nicross <nicross@users.noreply.github.com>
2019-10-05 11:32:00 -0500

    Add createNoiseMachine() utility

--- js/utility.js
+++ js/utility.js
@@ -1,3 +1,29 @@
+function createNoiseMachine() {
+  const context = audio.context(),
+    source = context.createBufferSource()
+
+  source.buffer = audio.noiseBuffer()
+  source.loop = true
+  source.start()
+
+  const filter = context.createBiquadFilter()
+  filter.frequency.value = context.sampleRate / 2
+  filter.Q.value = 0
+  filter.type = 'lowpass'
+
+  const output = context.createGain()
+  output.gain.value = 1
+
+  source.connect(filter)
+  filter.connect(output)
+
+  return {
+    filter,
+    output,
+    source,
+  }
+}
+
 function distance(x1, y1, x2, y2) {
   return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))
 }
