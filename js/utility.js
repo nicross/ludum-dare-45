de8b33e7379dd467eebe3d1495023f3f76c73f0d
nicross <nicross@users.noreply.github.com>
2019-10-05 19:36:47 -0500

    Add shuffle() utility

--- js/utility.js
+++ js/utility.js
@@ -49,6 +49,17 @@ function randomValue(array) {
   ]
 }
 
+function shuffle(array) {
+  array = array.slice()
+
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]
+  }
+
+  return array
+}
+
 function solveAngle(ax, ay, bx, by, cx, cy) {
   const a = distance(bx, by, cx, cy),
     b = distance(ax, ay, cx, cy),
