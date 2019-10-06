1d1dd2d4d03de47a826b5eebc87af21197da9727
nicross <nicross@users.noreply.github.com>
2019-10-06 10:56:58 -0500

    Soften compass

--- js/object/compass.js
+++ js/object/compass.js
@@ -5,8 +5,11 @@ const compass = inventObject({
     this.masterGain.gain.linearRampToValueAtTime(0.125, audio.time(1))
     this.masterPan.pan.linearRampToValueAtTime(0, audio.time(1))
 
+    this.noise.filter.Q.value = 4
+    this.noise.filter.type = 'bandpass'
+
     this.isRampingNoiseGain = true
-    this.noise.output.gain.linearRampToValueAtTime(0.125, audio.time(1))
+    this.noise.output.gain.linearRampToValueAtTime(0.0625, audio.time(1))
     setTimeout(() => this.isRampingNoiseGain = false, 1000)
   },
   onSpawn: function () {
@@ -25,8 +28,8 @@ const compass = inventObject({
       this.noise.filter.frequency.value = frequency
 
       if (!this.isRampingNoiseGain) {
-        let gain = (1 - strength) ** 4
-        gain = 0.125 + (gain * 0.25)
+        let gain = (1 - strength) ** 8
+        gain = 0.0625 + (gain * 0.125)
         this.noise.output.gain.value = gain
       }
     }
