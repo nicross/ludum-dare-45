dddcaa10662019a09496b11de1ace8eb41e77dd9
nicross <nicross@users.noreply.github.com>
2019-10-09 00:15:58 -0500

    Make ambiance density a factor of position grid distance

--- js/ambiance.js
+++ js/ambiance.js
@@ -1,15 +1,15 @@
 'use strict'
 
 const ambiance = (function IIFE() {
-  const maxSpawns = 24
+  const maxSpawns = 64
   const spawned = {}
 
   function hasSpawned(x, y) {
     return x in spawned && y in spawned[x]
   }
 
-  function spawnAmbiance() {
-    const count = Math.round(Math.random() * maxSpawns)
+  function spawnAmbiance(d) {
+    const count = Math.round(Math.random() * Math.max(4 * d, maxSpawns))
 
     for (let i = 0; i < count; i++) {
       spawn(randomValue(ambients), nextSpawnLocation(50, Math.PI, -Math.PI / 2))
@@ -19,21 +19,16 @@ const ambiance = (function IIFE() {
   return {
     activate: function () {
       spawned[0] = {0: true}
-
-      for (let i = 0; i < maxSpawns; i++) {
-        spawnFrom(ambients, nextSpawnLocation(25, 2 * Math.PI, 0))
-      }
-
       return this
     },
-    update: ({x, y}) => {
+    update: ({d, x, y}) => {
       if (!hasSpawned(x, y)) {
         if (!spawned[x]) {
           spawned[x] = {}
         }
 
         spawned[x][y] = true
-        spawnAmbiance()
+        spawnAmbiance(d)
       }
 
       return this
