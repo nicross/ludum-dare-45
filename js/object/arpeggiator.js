c7d746b1d1c36bfa1ed4e5183bb6068cf1ad39af
nicross <nicross@users.noreply.github.com>
2019-10-09 22:10:23 -0500

    Update arpeggiator.js

--- js/object/arpeggiator.js
+++ js/object/arpeggiator.js
@@ -1,5 +1,8 @@
+'use strict'
+
 // TODO: Main quest reward - When player finds all three synths, spawn this collectible at (a0,0)
 // On step, play the next note in the sequence: from the current chord (all notes), +/-1 from previous note in sequence
+
 const arpeggiator = inventObject({
   isCollectible: true,
 })
